0cac44fcf3409a5d5ba04ec65b8a9bbb1778d771
alusamuel <s.rurangami@alustudent.com>
2026-02-21 11:04:57 +0200

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -30,3 +30,27 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+            
+    def pmf(self, k):
+        """Calculates the PMF for a given number of successes.
+
+        Args:
+            k (int): number of successes
+
+        Returns:
+            float: PMF value for k
+        """
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        # factorial of k
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # approximate e^(-lambtha)
+        e = 2.7182818285
+        numerator = (self.lambtha ** k) * (e ** (-self.lambtha))
+        return numerator / factorial

313f62066616436437a5e62c963be0b0f05232f2
alusamuel <s.rurangami@alustudent.com>
2026-03-08 22:26:32 +0200

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Convolution on images with channels."""
 import numpy as np
-from math import ceil, floor
 
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
@@ -17,34 +16,44 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     m, h, w, c = images.shape
     kh, kw, kc = kernel.shape
     sh, sw = stride
+
     if kc != c:
         raise ValueError("Kernel channels must match image channels")
 
     if padding == 'valid':
-        ph = pw = 0
+        ph = 0
+        pw = 0
     elif padding == 'same':
-        ph = ceil(((h - 1) * (sh - 1) + kh - sh) / 2)
-        pw = ceil(((w - 1) * (sw - 1) + kw - sw) / 2)
+        out_h = (h + sh - 1) // sh
+        out_w = (w + sw - 1) // sw
+
+        ph_total = max((out_h - 1) * sh + kh - h, 0)
+        pw_total = max((out_w - 1) * sw + kw - w, 0)
+
+        ph = ph_total // 2
+        pw = pw_total // 2
     else:
         ph, pw = padding
 
-    padded = np.pad(
+    images_padded = np.pad(
         images,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
         mode='constant'
     )
-    h_p = h + 2 * ph
-    w_p = w + 2 * pw
 
-    out_h = floor((h_p - kh) / sh) + 1
-    out_w = floor((w_p - kw) / sw) + 1
+    h_p = images_padded.shape[1]
+    w_p = images_padded.shape[2]
+
+    out_h = (h_p - kh) // sh + 1
+    out_w = (w_p - kw) // sw + 1
     output = np.zeros((m, out_h, out_w))
 
     for i in range(out_h):
         for j in range(out_w):
             i_start = i * sh
             j_start = j * sw
-            window = padded[:, i_start:i_start + kh, j_start:j_start + kw, :]
+            window = images_padded[:, i_start:i_start + kh,
+                                   j_start:j_start + kw, :]
             output[:, i, j] = np.sum(window * kernel, axis=(1, 2, 3))
 
     return output

8754b0721bce8bc0351179933cc1d0d64ea5c1ce
s.rurangami@alustudent.com <s.rurangami@gmail.com>
2026-05-24 18:33:32 +0200

    Fix convolutional autoencoder architecture

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -30,40 +30,21 @@ def autoencoder(input_dims, filters, latent_dims):
         )(x)
         x = keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(x)
 
-    # Latent representation
-    # latent_dims is a tuple; we ensure the shape matches by using Conv2D
-    latent = keras.layers.Conv2D(
-        filters=latent_dims[2],
-        kernel_size=(3, 3),
-        padding='same',
-        activation='relu'
-    )(x)
-    encoder = keras.Model(inputs, latent, name='encoder')
+    encoder = keras.Model(inputs, x, name='encoder')
 
     # Decoder
     latent_inputs = keras.Input(shape=latent_dims)
     x = latent_inputs
 
-    # For the decoder, we mirror the encoder structure, using Conv2D + UpSampling2D
     for i, f in enumerate(reversed(filters)):
+        padding = 'valid' if i == len(filters) - 1 else 'same'
         x = keras.layers.Conv2D(
             filters=f,
             kernel_size=(3, 3),
-            padding='same',
+            padding=padding,
             activation='relu'
         )(x)
-        # All but the last two convs use same padding + upsampling
-        if i < len(filters) - 2:
-            x = keras.layers.UpSampling2D(size=(2, 2))(x)
-        elif i == len(filters) - 2:
-            # second to last conv: valid padding + upsample
-            x = keras.layers.Conv2D(
-                filters=f,
-                kernel_size=(3, 3),
-                padding='valid',
-                activation='relu'
-            )(x)
-            x = keras.layers.UpSampling2D(size=(2, 2))(x)
+        x = keras.layers.UpSampling2D(size=(2, 2))(x)
 
     # Last conv: same number of channels as input, sigmoid, no upsampling
     outputs = keras.layers.Conv2D(

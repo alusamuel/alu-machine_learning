04eced10eea69177acadadcee7c8ddf56cb34f4c
s.rurangami@alustudent.com <s.rurangami@gmail.com>
2026-05-31 05:08:13 +0200

    Fix pycodestyle in 15-main

--- supervised_learning/optimization/15-main.py
+++ supervised_learning/optimization/15-main.py
@@ -4,14 +4,16 @@ import numpy as np
 import tensorflow as tf
 model = __import__('15-model').model
 
+
 def one_hot(Y, classes):
     """convert an array to a one-hot matrix"""
     oh = np.zeros((Y.shape[0], classes))
     oh[np.arange(Y.shape[0]), Y] = 1
     return oh
 
+
 if __name__ == '__main__':
-    lib= np.load('../data/MNIST.npz')
+    lib = np.load('../data/MNIST.npz')
     X_train_3D = lib['X_train']
     Y_train = lib['Y_train']
     X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
@@ -26,6 +28,7 @@ if __name__ == '__main__':
 
     np.random.seed(0)
     tf.set_random_seed(0)
-    save_path = model((X_train, Y_train_oh), (X_valid, Y_valid_oh), layer_sizes,
-                                 activations, save_path='./model.ckpt')
+    save_path = model(
+        (X_train, Y_train_oh), (X_valid, Y_valid_oh), layer_sizes,
+        activations, save_path='./model.ckpt')
     print('Model saved in path: {}'.format(save_path))
